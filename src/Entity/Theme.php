d9fb5a66eaad06ce98f693e7be87fd6de58cfd3c
javad <javadsaeid8@gmail.com>
2025-02-05 13:32:25 +0330

    add api methods to Api resource + simple controller to test repository

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -3,16 +3,14 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Metadata\ApiFilter;
-use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 
 #[ORM\Entity]
 #[ApiResource(
@@ -20,27 +18,31 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
         new Get(),
         new GetCollection(),
         new Post(),
-        new Patch(),
-        new Delete()
-    ]
+        new Put(),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['theme:read']],
+    denormalizationContext: ['groups' => ['theme:write']],
 )]
-#[ApiFilter(BooleanFilter::class, properties: ['isDefault'])]
-#[ApiFilter(SearchFilter::class, properties: ['name' => 'partial'])]
 class Theme
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['theme:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
+    #[Groups(['theme:read', 'theme:write'])]
     private string $name;
 
-    #[ORM\Column(type: "json", nullable: true)]
+    #[ORM\Column(type: 'json', nullable: true)]
+    #[Groups(['theme:read', 'theme:write'])]
     private ?array $colors = null;
 
-    #[ORM\Column(type: "boolean")]
+    #[ORM\Column(options: ['default' => false])]
+    #[Groups(['theme:read', 'theme:write'])]
     private bool $isDefault = false;
 
     public function getId(): ?int
@@ -48,19 +50,15 @@ class Theme
         return $this->id;
     }
 
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-
     public function getName(): string
     {
         return $this->name;
     }
 
-    public function setName(string $name): void
+    public function setName(string $name): self
     {
         $this->name = $name;
+        return $this;
     }
 
     public function getColors(): ?array
@@ -68,19 +66,20 @@ class Theme
         return $this->colors;
     }
 
-    public function setColors(?array $colors): void
+    public function setColors(?array $colors): self
     {
         $this->colors = $colors;
+        return $this;
     }
 
-    public function isDefault(): bool
+    public function getIsDefault(): bool
     {
         return $this->isDefault;
     }
 
-    public function setIsDefault(bool $isDefault): void
+    public function setIsDefault(bool $isDefault): self
     {
         $this->isDefault = $isDefault;
+        return $this;
     }
-
 }
